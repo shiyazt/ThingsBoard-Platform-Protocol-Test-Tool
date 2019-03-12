ec153a2d798325f4db8985ba14e1a29fc6f08130
Shiyaz T <39874957+shiyazt@users.noreply.github.com>
2019-03-12 11:06:55 +0530

    Update ProtocolTester.py

--- src/ProtocolTester.py
+++ src/ProtocolTester.py
@@ -1,3 +1,22 @@
+""" 
+    ThingsBoard-Platform-Protocol-Test-Tool, Using this application user
+    can able to test CoAP, MQTT & HTTP Protocol with ThingsBoard Instance.
+    Copyright (C) 2019  Shiyaz T
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>. 
+    """
+
 import sys
 from PyQt5 import QtCore, QtGui, uic 
 from PyQt5.QtCore import QFileInfo
